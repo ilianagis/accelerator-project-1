6cbe00e3893a0431780da4c0ae30b52dd2fd2d06
Iliana Gavrilova <ilianagis98@gmail.com>
2024-07-26 22:07:32 +0300

    feat: вносит стили, правки

--- source/js/main.js
+++ source/js/main.js
@@ -1,4 +1,31 @@
 // https://swiperjs.com/get-started#installation
-// import Swiper from "swiper";
-// import {Navigation, Pagination} from "swiper/modules";
-// import 'swiper/css';
+import Swiper from 'swiper';
+import { Navigation, Pagination } from 'swiper/modules';
+import 'swiper/css';
+import { loadVideo } from './modules/about.js';
+
+document.addEventListener('DOMContentLoaded', () => {
+  const swiper = new Swiper('.swiper-container', {
+    modules: [Navigation, Pagination],
+    loop: true,
+    autoplay: {
+      delay: 2500,
+      disableOnInteraction: false,
+    },
+    pagination: {
+      el: '.swiper-pagination',
+      clickable: true,
+    },
+    navigation: {
+      nextEl: '.swiper-button-next',
+      prevEl: '.swiper-button-prev',
+    },
+  });
+});
+
+document.getElementById('play-button').addEventListener('click', () => {
+  const playButton = document.getElementById('play-button');
+
+  playButton.style.display = 'none';
+  loadVideo('9TZXsZItgdw');
+});
---
+++ source/js/modules/about.js
@@ -0,0 +1,24 @@
+/* <div video container - video></div>
+<pic><img id thumbnail - thumbnail></img></pic>
+<button id play-btn - id play-button></button>
+<div id video-placeholder - id video-placeholder></div> */
+
+export function loadVideo(videoID) {
+  const thumbnail = document.getElementById('video-thumbnail');
+  thumbnail.style.display = 'block';
+
+  const videoPlaceholder = document.getElementById('video-placeholder');
+  videoPlaceholder.style.display = 'block';
+
+  const videoFrame = document.createElement('iframe');
+  videoFrame.setAttribute('src', `https://www.youtube.com/embed/${videoID}?autoplay=1`);
+  videoFrame.setAttribute('frameborder', '0');
+  videoFrame.setAttribute('allow', 'accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture');
+  videoFrame.setAttribute('allowfullscreen', true);
+  videoFrame.style.position = 'absolute';
+  videoFrame.style.top = '0';
+  videoFrame.style.left = '0';
+  videoFrame.style.zIndex = '2';
+
+  videoPlaceholder.appendChild(videoFrame);
+}
